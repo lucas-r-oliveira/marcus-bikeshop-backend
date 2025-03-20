090e7eeee3680696b0443285c1baff5c65ed5ffa
lucas-r-oliveira <lucasoliveira7c@gmail.com>
2025-03-20 19:00:30 +0000

    product api changes

--- product/api.py
+++ product/api.py
@@ -13,31 +13,47 @@ class PartOptionResponse(BaseModel):
     name: str
     in_stock: bool
 
+    class Config:
+        from_attributes=True
+
 class ProductPartResponse(BaseModel):
     id: UUID
     name: str
     options: list[PartOptionResponse]
 
+    class Config:
+        from_attributes=True
+
+class PartConfigurationResponse(BaseModel):
+    id: UUID
+    part_id: UUID
+    available_options: list[PartOptionResponse]
+
+    class Config:
+        from_attributes=True
+
 class ProductResponse(BaseModel):
     id: UUID
     name: str
     description: str
-    base_price: float
-    currency: str
+    base_price: Money
+    currency: Money = Money(0, currency="EUR")
     image_url: str
     category: str
     parts: list[ProductPartResponse]
-    # TODO: part configs
-    # parts
+    part_configs: list[PartConfigurationResponse]
     # type
 
+    class Config:
+        from_attributes=True
+
     @field_serializer("base_price")
-    def serialize_base_price(self, base_price: Money):
-        return base_price.amount
+    def serialize_base_price(self, money: Money):
+        return money.amount
 
     @field_serializer("currency")
-    def serialize_currency(self, currency: Money):
-        return currency.amount
+    def serialize_currency(self, money: Money):
+        return money.currency
 
 class CreateProductRequest(BaseModel):
     name: str
@@ -46,7 +62,7 @@ class CreateProductRequest(BaseModel):
     currency: str
     image_url: str
     category: str
-    parts: list[PartConfiguration]  #FIXME:
+    parts: list[PartOptionSelection]  
     # type
 
 
@@ -73,21 +89,25 @@ def create_product_bp(session_factory):
         tags=["products", "bicycles"],
         success_status_code=200,
     )
-    def get_all_bicycle_products() -> list[dict]:#list[ProductResponse]: #FIXME:
+    def get_all_bicycle_products() -> list[ProductResponse]: 
         service = get_service()
+        products = service.list_products()
 
-        return [
-            ProductResponse(
-                id=product.id, 
-                name=product.name, 
-                description=product.description, 
-                base_price=product.base_price, #type: ignore
-                currency=product.base_price, #type: ignore
-                image_url=product.image_url,
-                # parts=product.parts,
-                category=product.category
-            ).model_dump() for product in service.list_products()
-        ]
+        # return [
+        #     ProductResponse(
+        #         id=product.id, 
+        #         name=product.name, 
+        #         description=product.description, 
+        #         base_price=product.base_price.amount, 
+        #         currency=product.base_price.currency, 
+        #         image_url=product.image_url,
+        #         parts=product.parts, 
+        #         part_configs=product.part_configs, #type: ignore
+        #         category=product.category
+        #     ).model_dump() for product in service.list_products()
+        # ]
+
+        return [ProductResponse.model_validate(product).model_dump() for product in products]
 
     @product_bp.post("/bicycles") # type: ignore
     @pydantic_api(
